c728ddaac5aeafabb682c5ec3e8dcb22295ea1d6
Илья Соцкий <krech.krech@outlook.com>
2019-12-07 23:23:44 +0500

    Добавьте файлы проекта.

---
+++ Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Script
+    {
+        static bool CheckedEmail(string email)
+        {
+
+        }
+    }
+
+}
